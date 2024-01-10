953537831d221af1143136aea010fa796f625cf0
liangchaofei <liangchaofei@liangchaofeideMacBook-Pro.local>
2024-01-10 10:10:24 +0800

    feat: init

--- .dumirc.ts
+++ .dumirc.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     devServer: {
       port: 1998 // 自定义端口号
     },
-    base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
-    publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+    base: `/${repo}/`,
+    publicPath:`/${repo}/`,
   },
 });
